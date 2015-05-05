91fb140fdb8c790b2aa09433c56290cbe2bea006
Danilo Brito <danilo.britoxd@hotmail.com>
2015-05-04 22:44:00 -0300

    Create 03.c

---
+++ Lista-7/03.c
@@ -0,0 +1,9 @@
+3­ Implemente uma função que crie uma cópia de uma lista encadeada. O protótipo da função deve ser dado por: Lista* copia (Lista* l);
+
+Lista* copia (Lista* l)
+{
+   Lista*aux = (Lista*) malloc(sizeof(Lista));
+   aux->info = l->info;
+   aux->prox = copia(l->prox);   
+   return aux;   
+}
