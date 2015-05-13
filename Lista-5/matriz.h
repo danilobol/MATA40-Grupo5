1a1c97ad7dac105acfb07489bcc687d38e962012
Danilo Brito <danilo.britoxd@hotmail.com>
2015-05-13 10:54:15 -0300

    Update matriz.h

--- Lista-5/matriz.h
+++ Lista-5/matriz.h
@@ -1,5 +1,8 @@
 #include "matriz.c"
 //Assinatura das funções
-matriz inicializarMatriz(matriz m);
-matriz adicionarValores(matriz m);
-void imprimeMatriz(matriz m);
+Matriz* criar(int m, int n);
+void liberar(Matriz* mat);
+float acessa(Matriz* mat, int i, int j);
+void atribuir(Matriz* mat, int i, int j, float num);
+int linhas(Matriz* mat);
+int colunas(Matriz* mat);
