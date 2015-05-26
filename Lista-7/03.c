cd65d615062dba7d8906304d34339957ef27a263
Danilo Brito <danilo.britoxd@hotmail.com>
2015-05-26 15:23:13 -0300

    Update 03.c

--- Lista-7/03.c
+++ Lista-7/03.c
@@ -2,8 +2,8 @@
 da função deve ser dado por: Lista* copia (Lista* l);
 
 Lista* copia (Lista* l){
-Lista*aux = (Lista*) malloc(sizeof(Lista));
-aux->info = l->info;
-aux->prox = copia(l->prox);
-return aux;
+  Lista*aux = (Lista*) malloc(sizeof(Lista));
+  aux->info = l->info;
+  aux->prox = copia(l->prox);
+  return aux;
 }
